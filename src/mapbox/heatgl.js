8e2e135ad36311ca9d3f7077268028f2fd21cfa8
UnderSilence <UnderSilentEdge@Gmail.com>
2021-01-13 12:21:38 +0800

    move heatmap display's mercator projection to shader

--- src/mapbox/heatgl.js
+++ src/mapbox/heatgl.js
@@ -60,18 +60,42 @@ export default class HeatGL {
     const { gl } = this;
     // this.resolution = newRes;
     this._res = Math.floor(newRes);
-
     // each grid point have (x, y, z, w) four comps
-    // this.gridIndices = new Float32Array(this._numVertices);
-    // for (let i = 0; i < this._numVertices; i++) {
-    //   this.gridIndices[i] = i;
-    // }
-    // this.gridIndexBuffer = util.createBuffer(gl, this.gridIndices);
-
-    const gridData = [];
+    const gridIndices = new Float32Array(this._res * this._res);
     const trianglesIndex = [];
+    const linesIndex = [];
+    const gridData = [];
     this._numGrids = 0;
     this._numTriangles = 0;
+    this._numLines = 0;
+
+    for (let i = 0, k = 0; i < this._res; i++) {
+      for (let j = 0; j < this._res; j++, k++) {
+        gridIndices[k] = k;
+        if (k > this._res && (k % this._res) !== 0) {
+          const a = k - this._res - 1;
+          const b = k - this._res;
+          const c = k - 1;
+          const d = k;
+          trianglesIndex.push(a, b, c, d, c, b);
+          this._numTriangles += 6;
+
+          linesIndex.push(c, d, b, d);
+          this._numLines += 4;
+          // in first row
+          if (k < this._res + this._res) {
+            linesIndex.push(a, b);
+            this._numLines += 2;
+          }
+          // in first col
+          if ((k % this._res) === 1) {
+            linesIndex.push(a, c);
+            this._numLines += 2;
+          }
+        }
+      }
+    }
+    /*
     for (let i = 0, k = 0; i < this._res; i++) {
       const v = i / (this._res - 1);
       const lat = util.lerp(LAT_MAX, LAT_MIN, v);
@@ -93,14 +117,30 @@ export default class HeatGL {
             const d = k;
             trianglesIndex.push(a, b, c, d, c, b);
             this._numTriangles += 6;
+
+            linesIndex.push(c, d, b, d);
+            this._numLines += 4;
+            // in first row
+            if (k < this._res + this._res) {
+              linesIndex.push(a, b);
+              this._numLines += 2;
+            }
+            // in first col
+            if ((k % this._res) === 1) {
+              linesIndex.push(a, c);
+              this._numLines += 2;
+            }
           }
           // console.log(`diff(${j / (this._res - 1) - res.x}, ${i / (this._res - 1) - res.y})`);
         }
       }
     }
-    console.log(new Uint16Array(trianglesIndex));
+    */
+
+    this.gridIndexBuffer = util.createBuffer(gl, gridIndices);
     this.gridDataBuffer = util.createBuffer(gl, new Float32Array(gridData));
     this.trianglesIndexBuffer = util.createIndexBuffer(gl, new Uint32Array(trianglesIndex));
+    this.linesIndexBuffer = util.createIndexBuffer(gl, new Uint32Array(linesIndex));
     // console.log(new Float32Array(this.gridPos));
   }
 
@@ -170,13 +210,13 @@ export default class HeatGL {
     // util.bindAttribute(gl, this.gridIndexBuffer, shader.a_index, 1);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, this.gridDataBuffer);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.trianglesIndexBuffer);
 
-    gl.enableVertexAttribArray(shader.a_pos);
-    gl.enableVertexAttribArray(shader.a_coords);
-    gl.vertexAttribPointer(shader.a_pos, 2, gl.FLOAT, false, 4 * 4, 0);
-    gl.vertexAttribPointer(shader.a_coords, 2, gl.FLOAT, false, 4 * 4, 4 * 2);
-
+    // gl.enableVertexAttribArray(shader.a_pos);
+    // gl.enableVertexAttribArray(shader.a_coords);
+    gl.enableVertexAttribArray(shader.a_index);
+    // gl.vertexAttribPointer(shader.a_pos, 2, gl.FLOAT, false, 4 * 4, 0);
+    // gl.vertexAttribPointer(shader.a_coords, 2, gl.FLOAT, false, 4 * 4, 4 * 2);
+    util.bindAttribute(gl, this.gridIndexBuffer, shader.a_index, 1);
     util.bindTexture(gl, this.colorRampTexture, 2);
 
     gl.uniform1f(shader.u_scale, this.scale);
@@ -193,17 +233,18 @@ export default class HeatGL {
 
     // console.log(`drawArrays length:${this.heatData.length}`);
     if (this.showMesh) {
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.trianglesIndexBuffer);
       console.log(`_numTriangles:${this._numTriangles}`);
       // gl.drawElements(gl.LINES, this._numTriangles, gl.UNSIGNED_SHORT, 0);
-
       // @MetaRu
       // need open WebGL extensions 'OES_element_index_uint'
       // to supprt gl.UNSIGNED_INT indexBuffer
       // gl.drawElements(gl.LINE_STRIP, this._numTriangles, gl.UNSIGNED_INT, 0);
       gl.drawElements(gl.TRIANGLES, this._numTriangles, gl.UNSIGNED_INT, 0);
     } else {
-      console.log(`_numGrids:${this._numGrids}`);
-      gl.drawArrays(gl.POINTS, 0, this._numGrids);
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.linesIndexBuffer);
+      console.log(`_numLines:${this._numLines}`);
+      gl.drawElements(gl.LINES, this._numLines, gl.UNSIGNED_INT, 0);
     }
   }
 
--- src/mapbox/shaders/heat.vert.glsl
+++ src/mapbox/shaders/heat.vert.glsl
@@ -1,13 +1,13 @@
 precision mediump float;
 #define M_PI 3.1415926535897932384626433832795
 
-attribute vec2 a_pos;
-attribute vec2 a_coords;
+// attribute vec2 a_pos;
+// attribute vec2 a_coords;
+attribute float a_index;
 
 uniform float u_scale;
 uniform float u_res;
 uniform mat4 u_matrix;
-uniform float u_max_heat;
 uniform sampler2D u_heat;
 
 varying vec2 v_coords;
@@ -24,9 +24,15 @@ vec2 lngLat2NormWebMercator(vec2 lngLat) {
 }
 
 void main() {
-  v_coords = a_coords;
+  v_coords = vec2(
+        fract(a_index / u_res),
+        floor(a_index / u_res) / u_res);
+
+  vec2 lnglat_coords = texcoords2LngLat(v_coords);
+  lnglat_coords.y = clamp(lnglat_coords.y, -85.05112877980659, 85.05112877980659);
+  vec2 mercator_coords = lngLat2NormWebMercator(lnglat_coords);
 
   vec4 heat = texture2D(u_heat, v_coords);
   gl_PointSize = 2.0;
-  gl_Position = u_matrix * vec4(a_pos, u_scale * heat.x, 1.0);
+  gl_Position = u_matrix * vec4(mercator_coords, u_scale * heat.x, 1.0);
 }
